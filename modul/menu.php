864a77561e34b4319c2fc0ad55fbe733796c863b
aqilaisir-pixel <aqilaisir@gmail.com>
2026-04-02 07:39:30 +0700

    first commit

---
+++ modul/menu.php
@@ -0,0 +1,15 @@
+ <!-- Menu -->
+    <div class="bg-white border-b">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="flex space-x-8 overflow-x-auto py-4">
+                <a href="#" class="text-gray-600 hover:text-indigo-600 whitespace-nowrap font-medium">Elektronik</a>
+                <a href="#" class="text-gray-600 hover:text-indigo-600 whitespace-nowrap font-medium">Fashion Pria</a>
+                <a href="#" class="text-gray-600 hover:text-indigo-600 whitespace-nowrap font-medium">Fashion Wanita</a>
+                <a href="#" class="text-gray-600 hover:text-indigo-600 whitespace-nowrap font-medium">Sepatu</a>
+                <a href="#" class="text-gray-600 hover:text-indigo-600 whitespace-nowrap font-medium">Aksesoris</a>
+                <a href="#" class="text-gray-600 hover:text-indigo-600 whitespace-nowrap font-medium">Kesehatan</a>
+                <a href="#" class="text-gray-600 hover:text-indigo-600 whitespace-nowrap font-medium">Kecantikan</a>
+                <a href="#" class="text-gray-600 hover:text-indigo-600 whitespace-nowrap font-medium">Rumah Tangga</a>
+            </div>
+        </div>
+    </div>  
