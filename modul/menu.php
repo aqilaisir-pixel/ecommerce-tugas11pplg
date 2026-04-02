e2c355164c56c4b51a7828f20772ab8382212dae
lalacantik-hub <you@example.com>
2026-04-02 08:03:50 +0700

    menu dinamis

--- modul/menu.php
+++ modul/menu.php
@@ -1,15 +1,16 @@
- <!-- Menu -->
+<!-- Categories -->
     <div class="bg-white border-b">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div class="flex space-x-8 overflow-x-auto py-4">
-                <a href="#" class="text-gray-600 hover:text-indigo-600 whitespace-nowrap font-medium">Elektronik</a>
-                <a href="#" class="text-gray-600 hover:text-indigo-600 whitespace-nowrap font-medium">Fashion Pria</a>
-                <a href="#" class="text-gray-600 hover:text-indigo-600 whitespace-nowrap font-medium">Fashion Wanita</a>
-                <a href="#" class="text-gray-600 hover:text-indigo-600 whitespace-nowrap font-medium">Sepatu</a>
-                <a href="#" class="text-gray-600 hover:text-indigo-600 whitespace-nowrap font-medium">Aksesoris</a>
-                <a href="#" class="text-gray-600 hover:text-indigo-600 whitespace-nowrap font-medium">Kesehatan</a>
-                <a href="#" class="text-gray-600 hover:text-indigo-600 whitespace-nowrap font-medium">Kecantikan</a>
-                <a href="#" class="text-gray-600 hover:text-indigo-600 whitespace-nowrap font-medium">Rumah Tangga</a>
-            </div>
+            <div class="flex space-x-8 overflow-x-auto py-4"> 
+                <?php
+                $sqlmenu = $conn->query("SELECT * FROM categories");
+                while($datamenu = $sqlmenu->fetch_assoc()){
+                    ?>
+                      <a href="#" class="text-gray-600 hover:text-indigo-600 whitespace-nowrap font-medium">Elektronik</a>
+                    <?php
+                }
+                ?>
+              
+</div>
         </div>
-    </div>  
+    </div>
